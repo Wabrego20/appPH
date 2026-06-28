e83e29252917c9f7f7477791d5bf1bcae0c6ba89
William Abrego <w.abrego20@gmail.com>
2026-06-28 07:21:04 -0500

    conexion.php

---
+++ config/conexion.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$dbname = "rivera_conecta";
+$user = "root";
+$password = "";
+
+try {
+
+    $conexion = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die("Error de conexión: " . $e->getMessage());
+
+}
